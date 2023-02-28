00f43c2e62546cbaff6b0ac49cee73205fcfb9e6
Brinax <85452528+Brinax@users.noreply.github.com>
2023-02-28 20:26:26 +0100

    ajout des fonctions
    -développement + simplification de fonctions (début)

--- AlgebraicAnalysis.py
+++ AlgebraicAnalysis.py
@@ -45,8 +45,107 @@ def Derivate(aFunc):
     elif(aFunc.type=="arctan"):
         return Function("const",1)/(cf(1)+Function("x",None)**cf(2))*Derivate(aFunc.vars)
 
+def developFunc(aFunc):
+    aFunc=simplifyUselessSubFuncs(aFunc)
+    if(aFunc.type in ["const","x"]+BasicFunctionsNames):
+        return aFunc
+    aFunc.vars=[developFunc(aVar) for aVar in aFunc.vars]
+    if(aFunc.type=="+"):
+        return simplifyUselessSubFuncs(aFunc)
+    if(aFunc.type=="*"):#on applique la distributivité
+        numbOfAddsPerArg=[1]*len(aFunc.vars)
+        numberOfFinalvars=1 #le nombre de developpements que la fonction aura à la fin, par exemple 3*sin(x)*2*ln(3+2) ne peut pas être developpé par distributivité et la valeur restera 2, mais (3x+4)*x oui et la valeur sera 2*1 = 2, pour (a+b)*(c+d+e)*f la valeur sera 2*3*1=6, il y aura 6 arguments additionnés: a*c*f+a*d*f+a*e*f+b*c*f+b*d*f+b*e*f
+        for (index,anArg)in enumerate(aFunc.vars):
+            if(anArg.type=="+"):
+                numbOfAddsPerArg[index]=len(anArg.vars)
+                numberOfFinalvars*=len(anArg.vars)
+        if(numberOfFinalvars==1):#si on ne peut pas développer par distributivité
+            return aFunc
+        else:
+            combinations=getAllCombinations(numbOfAddsPerArg)
+            addVars=[]
+            for aCombination in combinations:
+                multVars=[]
+                for (index,value) in enumerate(aCombination):
+                    if(aFunc.vars[index].type!="+"):
+                        if(value!=0):
+                            print("valeur de l'indexe voulu erronée!")
+                        multVars.append(aFunc.vars[index])
+                    else:
+                        multVars.append(aFunc.vars[index].vars[value])
+                addVars.append(simplifyUselessSubFuncs(Function("*",multVars)))
+            return Function("+",addVars)
+    if(aFunc.type=="^"):
+        if(aFunc.vars[-1].type=="+"):
+            multVars=[]
+            for aVar in aFunc.vars[-1].vars:
+                multVars.append(Function("^",aFunc.vars[:-1]+[aVar]))
+            return Function("*",multVars)
+        else:
+            if(len(aFunc.vars)>2):
+                aFunc.vars[-2]*=aFunc.vars[-1]
+                return developFunc(Function("^",aFunc.vars[:-1]))
+            else:
+                return aFunc
+    return aFunc
+
+
+
+def getAllCombinations(arrayOfArraysLengths):
+    if(len(arrayOfArraysLengths)==1):
+        return [[numb]for numb in range(arrayOfArraysLengths[0])]
+    else:
+        returnArray=[]
+        for i in range(arrayOfArraysLengths[0]):
+            for aCombination in getAllCombinations(arrayOfArraysLengths[1:]):
+                returnArray.append([i]+aCombination)
+        return returnArray
+    
+def simplifyFunc(aFunc):
+    if(aFunc.type=="const" or aFunc.type=="x"):
+        return aFunc
+    if(aFunc.type not in BasicFunctionsNames): 
+        aFunc.vars=[simplifyFunc(aVar) for aVar in aFunc.vars]
+
+    if(aFunc.type=="+"):
+        newVars=[]
+        for aVar in aFunc.vars:
+            if(aVar.type!="const" or aVar.vars!=0):
+                newVars.append(aVar)
+        if(len(newVars)==0):
+            return cf(0)
+        elif(len(newVars)==1):
+            return newVars[0]
+        else:
+            return Function("+",newVars)
+    if(aFunc.type=="*"):
+        newVars=[]
+        for aVar in aFunc.vars:
+            if(aVar.type!="const"):
+                newVars.append(aVar)
+            elif(aVar.vars==0):
+                return cf(0)
+            elif(aVar.vars!=1):
+                newVars.append(aVar)
+        if(len(newVars)==0):
+            return cf(1)
+        elif(len(newVars)==1):
+            return newVars[0]
+        else:
+            return Function("*",newVars)
+        
+    return aFunc
+
+        
+def developAndSimplifyFunc(aFunc):
+    aFunc=developFunc(aFunc)
+    return simplifyFunc(aFunc)
+
+
+
+
+
 if __name__ =="__main__":
     while True:
-        inputFunc=textToFunc(input())
-        print(inputFunc)
-        print(Derivate(inputFunc))
+        print(getAllCombinations([5,2,2]))
+        input()

6760398ef7bfc44f0b5589f05e5dd86fc9fd41f0
Gustule <99535648+Gustule@users.noreply.github.com>
2023-03-20 09:58:15 +0100

    Fichier pas fonctionnel

--- méth. de Newton.py
+++ méth. de Newton.py
@@ -1,3 +1,5 @@
+# Fichier ne fonctionnant pas encore, travail en cours
+
 def derivee(f, h=10**-5):
     def f_prime(x):
         return (f(x+h)-f(x-h))/(2*h)
@@ -8,6 +10,10 @@ def derivee(f, h=10**-5):
 x = 0
 funct = "x**2 + 2*x + (x - 6)"
 
+def make_function_from_string(str_func, variable_name='x'):    # str_func: la fonction sous forme de string
+	return eval(f'lambda {variable_name}: {str_func}')    # On applique variable_name comme variable dans str_func grace à la fonction lambda
+
+
 def newton(funct):
     listdepoint = []
     for x in range (-100, 100):
@@ -16,16 +22,14 @@ def newton(funct):
     
     print(listdepoint)
     zero = []
-    j = 10**10
-    for i in listdepoint:
-        if j - i > abs(j): 
-            zero.append(j)
-        j = i
+    for index, valeur in enumerate(listdepoints):
+        if valeur * listdepoint[index+1] <= 0:    # Il y a un changement de signe entre les deux points
+            zeros.append(valeur)
     return zero
 
 
 for i in range(10):
-    point = newton(funct)
+    point = newton(funct)    # On obtient les zéros approximatifs
     print(derivee(funct(point)))
     funct = str(derivee(funct(point))) + "*x"
 

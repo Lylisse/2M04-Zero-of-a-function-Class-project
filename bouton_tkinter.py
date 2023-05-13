15470689bc104d341a7fd1b1163f6e5a9bbfb930
Lylisse <124567567+Lylisse@users.noreply.github.com>
2023-05-13 02:48:03 +0200

    Add files via upload

--- bouton_tkinter.py
+++ bouton_tkinter.py
@@ -2,7 +2,9 @@ from tkinter import *
 #on importe le module tkinter qui nous permettra de cliquer sur un bouton pour afficher le graphique et les racines de la fonction
 from tkinter import ttk
 #On importe un sous-module de tkinter pour pouvoir l'utiliser en dehors de l'objet
+from dichotomies import t0i_bracketing_dichotomie
 
+noerror = "no error detected"
 fcterror = "La fonction que vous avez entrée n'est pas correcte."
 intervalerror = "L'intervalle que vous avez entrée n'est pas correcte. (Les virgules ne sont pas acceptées.)"
 nomethodselecterror = "Aucune méthode n'a été sélectionnée."
@@ -10,13 +12,19 @@ nomethodselecterror = "Aucune méthode n'a été sélectionnée."
 methods_list = (5, 3, 2, 1)
 def calculate_zeros(method, function, interval):
 #on choisit quelle méthode utilisée puis envoyons en arguments la fonction et l'intervalle sous la forme suivante:"[{fonction}, {début de l'intervalle},  {fin de l'intervalle}]"
-    #try:
-    interval = [float(interval[0]), float(interval[1])]
-    #except:
-    #    errorLabel.config(text=intervalerror)
-    if method.get() == "method1":
-        pass
-    elif method.get() == "method2":
+    try:
+        if float(interval[1]) > float(interval[0]):
+            interval = [float(interval[0]), float(interval[1])]
+            errorLabel.config(text=noerror)
+        else:
+            raise Exception("")
+    except:
+        errorLabel.config(text=intervalerror)
+    if method == "method1":
+        global fct_zeros
+        fct_zeros = t0i_bracketing_dichotomie(function, interval[0], interval[1])
+        errorLabel.config(text=fct_zeros)
+    elif method == "method2":
         pass
     else:
         errorLabel.config(text=nomethodselecterror)
@@ -62,17 +70,17 @@ ttk.Label(mainframe, text="Intervalle").grid(column=4, row=2)
 
 methode_1_select = StringVar()
 
-ttk.Checkbutton(mainframe, text="méthode 1", variable=methode_1_select, onvalue="method1").grid(column=1, row=3)
+ttk.Checkbutton(mainframe, text="dichotomie", variable=methode_1_select, onvalue="method1").grid(column=1, row=3)
 
 
 ttk.Checkbutton(mainframe, text="méthode 2", variable=methode_1_select, onvalue="method2").grid(column=2, row=3)
 
 
-ttk.Button(mainframe, text="calculate_zeros", command=lambda:calculate_zeros(methode_1_select, fonction_entry, [debut_intervalle_entry.get(), fin_intervalle_entry.get()])).grid(column=1, row=4, sticky=W)
+ttk.Button(mainframe, text="calculate_zeros", command=lambda:calculate_zeros(methode_1_select.get(), fonction_entry.get(), [debut_intervalle_entry.get(), fin_intervalle_entry.get()])).grid(column=1, row=4, sticky=W)
 #le bouton permettant de lancer le calcul des zéros sera placé à un certain endroit
 
-errorLabel = ttk.Label(mainframe, text="no error detected")
-errorLabel.grid(row=4, column=3)
+errorLabel = ttk.Label(mainframe, text=noerror)
+errorLabel.grid(row=4, column=5)
 
 fonction_entry.focus()
 #ceci permet de faire en sorte que la souris aille directement dans l'entrée reservée à la fonction, pour ne pas avoir à bouger le curseur

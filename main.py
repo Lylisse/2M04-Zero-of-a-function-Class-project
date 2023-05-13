d8d37c88afa94f17b3fa20a1171f020cced136ae
Lylisse <124567567+Lylisse@users.noreply.github.com>
2023-05-13 13:29:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from InputInterpretation import *
 from AlgebraicAnalysis import *
-
+from bouton_tkinter import *
 
 while True:
     print(getRandomFunc(maxDepth=3,constFunc=lambda:randrange(-100,101)))
